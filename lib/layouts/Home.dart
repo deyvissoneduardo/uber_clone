09639e0b6b0cfcdf7f05352b3c989d1f9518f4fb
deyvisson eduardo <deyvissoneduardo22@gmail.com>
2020-08-04 14:59:09 -0300

    login interface

--- lib/layouts/Home.dart
+++ lib/layouts/Home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -6,8 +7,97 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  /** controladores **/
+  TextEditingController _controllerEmail = TextEditingController();
+  TextEditingController _controllerSenha = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('images/fundo.png'), fit: BoxFit.cover)),
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: 32),
+                  child: Image.asset(
+                    "images/logo.png",
+                    width: 200,
+                    height: 150,
+                  ),
+                ),
+                /** campo email **/
+                TextField(
+                  controller: _controllerEmail,
+                  autofocus: true,
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: 'e-mail',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6))),
+                ),
+                /** campo senha **/
+                TextField(
+                  controller: _controllerSenha,
+                  obscureText: true,
+                  keyboardType: TextInputType.visiblePassword,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: 'Senha',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6))),
+                ),
+                /** btn entrar **/
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10, top: 16),
+                  child: RaisedButton(
+                    child: Text(
+                      'Entrar',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    color: Color(0xff1ebbd8),
+                    padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    onPressed: () {},
+                  ),
+                ),
+                /** nao tem conta **/
+                Center(
+                  child: GestureDetector(
+                    child: Text(
+                      'Não tem conta? Cadastre-se',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+                /** msngs de error **/
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Center(
+                    child: Text(
+                      'Error',
+                      style: TextStyle(color: Colors.red, fontSize: 20),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
