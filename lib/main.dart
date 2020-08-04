3464c8466999dca477b6608384fda238f3dd45d9
deyvisson eduardo <deyvissoneduardo22@gmail.com>
2020-08-04 14:26:42 -0300

    project configuration

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:uber_clone/layouts/Home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Uber Clone',
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
